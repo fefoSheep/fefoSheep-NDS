ca2dd0972d1cf9501a3304ed7276fba1a15881a3
Liam <tholgaming@gmail.com>
2017-10-28 00:21:04 -0400

    :)

---
+++ source/main.c
@@ -0,0 +1,24 @@
+#include <nds.h>
+#include <stdio.h>
+int main()
+{
+	consoleDemoInit();
+	iprintf("v1.0\n");
+	iprintf("downloa fefoshep form gothu!!!\n");
+	swiDelay(100000000);
+	iprintf("downloa compete!! now instalin!\n");
+	swiDelay(100000000);
+	iprintf("isntal compete! nowa downloa ds friig am!!!\n");
+	swiDelay(100000000);
+	iprintf("now doanload swotch frii gam port for ds too!\n");
+	swiDelay(100000000);
+	iprintf("downlaod fri gam wii u & wii on ds\n");
+	swiDelay(100000000);
+	iprintf("now a doanloa PEECEE GAM\n");
+	swiDelay(100000000);
+	iprintf("getin gamcub gam\n");
+	swiDelay(100000000);
+	iprintf("u GO T  FRI GAM!!!\n");
+	swiDelay(100000000);
+	return 0;
+}
