e8172430ff6f5206108aa1c7349708ae31304917
Liam <tholgaming@gmail.com>
2018-04-27 11:09:19 -0400

    yeet

--- source/main.c
+++ source/main.c
@@ -3,28 +3,22 @@
 int main()
 {
 	consoleDemoInit();
-	iprintf("v1.7.0\n");
-	iprintf("downloa fefoshep form gothu!!!\n");
-	swiDelay(100000000);
-	iprintf("mow instalin!\n");
-	swiDelay(100000000);
-	iprintf("instaled fefshep!11\n");
-	swiDelay(100000000);
-	iprintf("now installign clongo exploit\n");
+	iprintf("v1.8.0\n")
+	iprintf("installign clongo exploit\n");
 	swiDelay(100000000);
 	iprintf("isntal compete! nowa downloa ds friig am!!!\n");
 	swiDelay(100000000);
-	iprintf("now doanload swotch frii gam port for ds too!\n");
+	iprintf("downlaod fri gam siwth and WEE u GAM\n");
 	swiDelay(100000000);
-	iprintf("downlaod fri gam wii u & wii on ds\n");
-	swiDelay(100000000);
-	iprintf("git frii gba gaem nao\n");
+	iprintf("git frii gba gaem nao wif gabcub\n");
 	swiDelay(100000000);
 	iprintf("now a doanloa PEECEE GAM\n");
 	swiDelay(100000000);
-	iprintf("getin gamcub gam\n");
-	swiDelay(100000000);
 	iprinf("now instal rednand yelownan for S T A B I L I T Y\n");
+	swiDelay(100000000);
+	iprintf("INTALLIENG FUSE THE JELLO FOR DNINTNEOD DS???!??!?\n");
+	swiDelay(100000000);
+	iprintf("eaten FRII JELLO game/????\n");
 	swiDelay(100000000);
 	iprintf("u GO T  FRI GAM!!!\n");
 	swiDelay(100000000);
